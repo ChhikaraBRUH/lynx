8b2bc8bfed8470065ba7d89634eba1b3a5e3e6c8
ChhikaraBRUH <chaitanyachhikara@gmail.com>
2024-01-19 00:02:09 +0530

    fix: minor fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Linx: Link Shortener',
+  title: 'Lynx: Link Shortener',
   description: 'A simple link shortener',
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="bg-clip-text text-center text-6xl font-bold text-blue-800 text-transparent">
-        Linx - The Simple Link Shortener
+        Lynx - The Simple Link Shortener
       </h1>
     </main>
   );
