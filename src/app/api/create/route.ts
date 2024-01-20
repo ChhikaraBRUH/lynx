b374d6997ee2e57bf41801061d8fab126602df06
ChhikaraBRUH <chaitanyachhikara@gmail.com>
2024-01-20 12:39:46 +0530

    chore: prettier

--- src/app/api/create/route.ts
+++ src/app/api/create/route.ts
@@ -1,40 +1,44 @@
-import z from "zod";
 import prisma from "@/db";
+import z from "zod";
 
 const createShortLinkSchema = z.object({
-    url: z.string().url(),
-    slug: z.string().min(3).max(100),
-}); 
+  url: z.string().url(),
+  slug: z.string().min(3).max(100),
+});
 
 export const POST = async (req: Request) => {
-    try {
+  try {
     const body = await req.json();
     const parsedBody = createShortLinkSchema.parse(body);
 
     const exisitingLink = await prisma.link.findUnique({
-        where: {
-            slug: parsedBody.slug,
-        }
+      where: {
+        slug: parsedBody.slug,
+      },
     });
 
     if (exisitingLink) {
-        throw new Error("his short link already exists!");
+      throw new Error("This short link already exists!");
     }
 
     const newLink = await prisma.link.create({
-        data: {
-            url: parsedBody.url,
-            slug: parsedBody.slug,
-        }
+      data: {
+        url: parsedBody.url,
+        slug: parsedBody.slug,
+      },
     });
 
-        return new Response(JSON.stringify({
-            message: "Short link created!",
-            url: newLink.url,
-            slug: newLink.slug,
-        }), {status:201});
-    } catch (error) {
-        return new Response(JSON.stringify({error: String(error)}), { status: 400 });
-    }
-}
-   
+    return new Response(
+      JSON.stringify({
+        message: "Short link created!",
+        url: newLink.url,
+        slug: newLink.slug,
+      }),
+      { status: 201 },
+    );
+  } catch (error) {
+    return new Response(JSON.stringify({ error: String(error) }), {
+      status: 400,
+    });
+  }
+};
