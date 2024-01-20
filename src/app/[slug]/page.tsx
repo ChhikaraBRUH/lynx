5c8dc238d29c7082e059ef5c64e69e749d9f052a
ChhikaraBRUH <chaitanyachhikara@gmail.com>
2024-01-20 13:15:08 +0530

    feat: add vercel KV caching

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,19 +1,34 @@
 import prisma from "@/db";
+import { kv } from "@vercel/kv";
 import { notFound, redirect } from "next/navigation";
 
-export default async function RedirectPage({ params }: {params: { slug: string }}) {
-    const { slug } = params;
-    const data = await prisma.link.findUnique({
-        where: {
-            slug,
-        },
+export default async function RedirectPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const { slug } = params;
+
+  let cache = await kv.get<string>(slug);
+
+  console.log({ cache });
+
+  if (!cache) {
+    const data = await prisma.link.findFirst({
+      where: {
+        slug,
+      },
     });
 
-    const {url} = data || {};
+    const { url } = data || {};
 
     if (!!url) {
-        redirect(url);
+      cache = url;
+      await kv.set(slug, url);
     } else {
-        notFound();
+      notFound();
     }
+  }
+
+  redirect(cache);
 }
